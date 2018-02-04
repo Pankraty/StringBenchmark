a8164d1389a7beab5e842f326c58b052508ec726
Aleksei <pankraty@gmail.com>
2018-02-04 13:17:11 +0400

    Added benchmark for range addresses

--- Program.cs
+++ Program.cs
@@ -9,14 +9,15 @@ namespace StringBenchmark
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<StringBenchmark>();
+            BenchmarkRunner.Run<CellBenchmark>();
+            BenchmarkRunner.Run<RangeBenchmark>();
 
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
         }
     }
 
-    public class StringBenchmark
+    public class CellBenchmark
     {
         public class Cell
         {
@@ -38,7 +39,7 @@ namespace StringBenchmark
             }
         }
         
-        [Benchmark(Description = "String.Format(...)")]
+        [Benchmark(Description = "String.Format(...)", Baseline = true)]
         public string TestFormat()
         {
             var cell = new Cell();
@@ -81,6 +82,81 @@ namespace StringBenchmark
         private string Builder(string col, int row)
         {
             return new StringBuilder("$").Append(col).Append("$").Append(row).ToString();
+        }
+    }
+
+
+    public class RangeBenchmark
+    {
+        public class Cell
+        {
+            static Random rnd = new Random();
+
+            readonly string[] columns = {
+                "A", "B", "C", "D", "E", "F",
+                "AB", "CD", "EF", "GH", "IJ", "KL",
+                "ABC", "DEF", "GHI", "JKL", "MNO", "PRQ" };
+
+            public string Col { get; set; }
+
+            public int Row { get; set; }
+
+            public Cell()
+            {
+                Col = columns[rnd.Next(columns.Length)];
+                Row = rnd.Next(1_000_000);
+            }
+        }
+
+        [Benchmark(Description = "String.Format(...)", Baseline = true)]
+        public string TestFormat()
+        {
+            var cell1 = new Cell();
+            var cell2 = new Cell();
+            return Format(cell1.Col, cell1.Row, cell2.Col, cell2.Row);
+        }
+
+        [Benchmark(Description = "String.Concat(...)")]
+        public string TestConcat()
+        {
+            var cell1 = new Cell();
+            var cell2 = new Cell();
+            return Concat(cell1.Col, cell1.Row, cell2.Col, cell2.Row);
+        }
+
+        [Benchmark(Description = "StringBuilder.ToString()")]
+        public string TestStringBuilder()
+        {
+            var cell1 = new Cell();
+            var cell2 = new Cell();
+            return Builder(cell1.Col, cell1.Row, cell2.Col, cell2.Row);
+        }
+
+        [Benchmark(Description = "String.Plus")]
+        public string TestPlus()
+        {
+            var cell1 = new Cell();
+            var cell2 = new Cell();
+            return Plus(cell1.Col, cell1.Row, cell2.Col, cell2.Row);
+        }
+
+        private string Plus(string col1, int row1, string col2, int row2)
+        {
+            return "$" + col1 + "$" + row1.ToString() + ":" + "$" + col2 + "$" + row2;
+        }
+        private string Format(string col1, int row1, string col2, int row2)
+        {
+            return string.Format("${0}${1}:${2}${3}", col1, row1, col2, row2);
+        }
+        private string Concat(string col1, int row1, string col2, int row2)
+        {
+            return String.Concat("$", col1, "$", row1, ":", "$", col2, "$", row2);
+        }
+        private string Builder(string col1, int row1, string col2, int row2)
+        {
+            return new StringBuilder("$").Append(col1).Append("$").Append(row1).Append(":")
+                .Append("$").Append(col2).Append("$").Append(row2)
+                .ToString();
         }
     }
 }
--- README.md
+++ README.md
@@ -4,10 +4,21 @@ _Everybody knows_ that string concatenation with a plus sign is a very inefficie
 But it turned out that it is not always true. If we are looking for the way of building a cell address in an "Excel-way", the most primitive way shows the best performance.
 
 ```
-                      Method |     Mean |     Error |    StdDev |
- --------------------------- |---------:|----------:|----------:|
-          String.Format(...) | 510.3 ns | 4.3134 ns | 3.8237 ns |
-          String.Concat(...) | 477.5 ns | 0.4184 ns | 0.3913 ns |
-    StringBuilder.ToString() | 400.2 ns | 1.0409 ns | 0.9227 ns |
-  "$"+col+"$"+row.ToString() | 357.3 ns | 1.3935 ns | 1.0076 ns |
+                     Method |     Mean |    Error |    StdDev | Scaled |
+--------------------------- |---------:|---------:|----------:|-------:|
+         String.Format(...) | 509.8 ns | 3.862 ns | 3.6127 ns |   1.00 |
+         String.Concat(...) | 477.3 ns | 3.119 ns | 2.9174 ns |   0.94 |
+   StringBuilder.ToString() | 399.4 ns | 1.109 ns | 1.0373 ns |   0.78 |
+ "$"+col+"$"+row.ToString() | 355.1 ns | 1.028 ns | 0.8582 ns |   0.70 |
+```
+
+Building cell range addresses ("$AB$100:$DE$200"), however, shows a different result. StringBuilder wins:
+
+```
+                   Method |     Mean |    Error |   StdDev | Scaled |
+------------------------- |---------:|---------:|---------:|-------:|
+       String.Format(...) | 958.2 ns | 3.888 ns | 3.637 ns |   1.00 |
+       String.Concat(...) | 964.7 ns | 6.498 ns | 5.761 ns |   1.01 |
+ StringBuilder.ToString() | 852.3 ns | 3.512 ns | 3.114 ns |   0.89 |
+              String.Plus | 891.9 ns | 1.813 ns | 1.696 ns |   0.93 |
 ```
